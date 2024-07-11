a7669f60487bb870097c23a22d7dfe2c571bc51b
mytja <mitja@severkar.eu>
2024-07-11 12:24:58 +0200

    separate structs

--- timetable.go
+++ timetable.go
@@ -5,6 +5,71 @@ import (
 	"time"
 )
 
+type AllDayEvent struct {
+	ID       int    `json:"id"`
+	Date     string `json:"date"`
+	Location struct {
+		Name      any `json:"name"`
+		ShortName any `json:"short_name"`
+	} `json:"location"`
+	// Teachers: TBD
+	Teachers []any  `json:"teachers"`
+	Name     string `json:"name"`
+	// EventType je:
+	// 4 – prazniki/počitnice
+	// 6 – celodnevne aktivnosti, ki jih določi šola
+	// ostalo – ????
+	EventType int `json:"event_type"`
+}
+
+type TimetableHour struct {
+	Time struct {
+		FromID int    `json:"from_id"`
+		ToID   int    `json:"to_id"`
+		Date   string `json:"date"`
+	} `json:"time"`
+	// Videokonferenca je useless
+	Videokonferenca struct {
+		ID      any `json:"id"`
+		Link    any `json:"link"`
+		Zacetek any `json:"zacetek"`
+		Opomba  any `json:"opomba"`
+	} `json:"videokonferenca"`
+	EventID int    `json:"event_id"`
+	Color   string `json:"color"`
+	Subject struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"subject"`
+	Completed bool `json:"completed"`
+	// HourSpecialType je lahko
+	// - exam
+	// - substitution
+	// - pre-exam (preverjanje)
+	// - cancelled (odpadla)
+	// - prazen string (eAsistent pošlje null)
+	//
+	// Zanimivo, da eAsistent ne podpira exama in substitutiona hkrati ... slab design choice ig
+	HourSpecialType string `json:"hour_special_type"`
+	Departments     []struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"departments"`
+	Classroom struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"classroom"`
+	Teachers []struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"teachers"`
+	Groups []struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"groups"`
+	Info []any `json:"info"`
+}
+
 type TimetableResponse struct {
 	// TimeTable nam pove vse ure zahtevanega tedna (in še preveč???). To je torej najbolj levi stolpec v eAsistent sistemu.
 	TimeTable []struct {
@@ -24,71 +89,10 @@ type TimetableResponse struct {
 		Date      string `json:"date"`
 	} `json:"day_table"`
 	// SchoolHourEvents nam pove dejanske ure (predmete) na urniku. Končno nekaj uporabnega.
-	SchoolHourEvents []struct {
-		Time struct {
-			FromID int    `json:"from_id"`
-			ToID   int    `json:"to_id"`
-			Date   string `json:"date"`
-		} `json:"time"`
-		// Videokonferenca je useless
-		Videokonferenca struct {
-			ID      any `json:"id"`
-			Link    any `json:"link"`
-			Zacetek any `json:"zacetek"`
-			Opomba  any `json:"opomba"`
-		} `json:"videokonferenca"`
-		EventID int    `json:"event_id"`
-		Color   string `json:"color"`
-		Subject struct {
-			ID   string `json:"id"`
-			Name string `json:"name"`
-		} `json:"subject"`
-		Completed bool `json:"completed"`
-		// HourSpecialType je lahko
-		// - exam
-		// - substitution
-		// - pre-exam (preverjanje)
-		// - cancelled (odpadla)
-		// - prazen string (eAsistent pošlje null)
-		//
-		// Zanimivo, da eAsistent ne podpira exama in substitutiona hkrati ... slab design choice ig
-		HourSpecialType string `json:"hour_special_type"`
-		Departments     []struct {
-			ID   string `json:"id"`
-			Name string `json:"name"`
-		} `json:"departments"`
-		Classroom struct {
-			ID   string `json:"id"`
-			Name string `json:"name"`
-		} `json:"classroom"`
-		Teachers []struct {
-			ID   string `json:"id"`
-			Name string `json:"name"`
-		} `json:"teachers"`
-		Groups []struct {
-			ID   string `json:"id"`
-			Name string `json:"name"`
-		} `json:"groups"`
-		Info []any `json:"info"`
-	} `json:"school_hour_events"`
+	SchoolHourEvents []TimetableHour `json:"school_hour_events"`
 	// Events: TBD
-	Events       []any `json:"events"`
-	AllDayEvents []struct {
-		ID       int    `json:"id"`
-		Date     string `json:"date"`
-		Location struct {
-			Name      any `json:"name"`
-			ShortName any `json:"short_name"`
-		} `json:"location"`
-		// Teachers: TBD
-		Teachers []any  `json:"teachers"`
-		Name     string `json:"name"`
-		// EventType je:
-		// 4 – prazniki/počitnice
-		// 6 – celodnevne aktivnosti, ki jih določi šola
-		// ostalo – ????
-		EventType int `json:"event_type"`
-	} `json:"all_day_events"`
+	Events       []any         `json:"events"`
+	AllDayEvents []AllDayEvent `json:"all_day_events"`
 }
 
 func (s *sessionImpl) GetTimetable(startDate time.Time, endDate time.Time) (TimetableResponse, error) {
