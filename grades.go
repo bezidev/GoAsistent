ae7866a84bc90d1a095ada694d771dd66515e2bd
mytja <mitja@severkar.eu>
2024-07-17 23:27:46 +0200

    smart guesses

--- grades.go
+++ grades.go
@@ -23,7 +23,7 @@ type GradesResponse struct {
 		ID         int    `json:"id"`
 		GradeType  string `json:"grade_type"`
 		IsExcused  bool   `json:"is_excused"`
-		FinalGrade struct {
+		FinalGrade *struct {
 			Value string `json:"value"`
 		} `json:"final_grade"`
 		AverageGrade string `json:"average_grade"`
@@ -32,14 +32,14 @@ type GradesResponse struct {
 			ID         int `json:"id"`
 			FinalGrade any `json:"final_grade"`
 			Grades     []struct {
-				TypeName     string `json:"type_name"`
-				Comment      any    `json:"comment"`
-				ID           int    `json:"id"`
-				Type         string `json:"type"`
-				OverridesIds any    `json:"overrides_ids"`
-				Value        string `json:"value"`
-				Color        string `json:"color"`
-				Date         string `json:"date"`
+				TypeName     string  `json:"type_name"`
+				Comment      *string `json:"comment"`
+				ID           int     `json:"id"`
+				Type         string  `json:"type"`
+				OverridesIds any     `json:"overrides_ids"`
+				Value        string  `json:"value"`
+				Color        string  `json:"color"`
+				Date         string  `json:"date"`
 			} `json:"grades"`
 		} `json:"semesters"`
 	} `json:"items"`
@@ -51,11 +51,11 @@ type SubjectGradesResponse struct {
 	ID         int    `json:"id"`
 	GradeType  string `json:"grade_type"`
 	IsExcused  bool   `json:"is_excused"`
-	FinalGrade struct {
+	FinalGrade *struct {
 		ID         int    `json:"id"`
 		Value      string `json:"value"`
 		Date       string `json:"date"`
-		InsertedBy struct {
+		InsertedBy *struct {
 			ID   int    `json:"id"`
 			Name string `json:"name"`
 		} `json:"inserted_by"`
@@ -75,7 +75,7 @@ type SubjectGradesResponse struct {
 			Date         string `json:"date"`
 			NotifiedAt   any    `json:"notified_at"`
 			InsertedAt   string `json:"inserted_at"`
-			InsertedBy   struct {
+			InsertedBy   *struct {
 				ID   int    `json:"id"`
 				Name string `json:"name"`
 			} `json:"inserted_by"`
